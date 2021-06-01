691bc04333723a8d1d70140829d36d6790367500
thanhit95 <thanh.it1995@gmail.com>
2021-06-01 16:38:48 +0700

    refactor: rename variable names; improve code

--- cpp-pthreads/ex01-max-div-a.cpp
+++ cpp-pthreads/ex01-max-div-a.cpp
@@ -10,27 +10,30 @@ using namespace std;
 
 
 int main() {
-    auto tpStart = mytool::HiResClock::now();
+    constexpr int RANGE_START = 1;
+    constexpr int RANGE_STOP = 50000;
 
-    int resInt = 0;
+    int resValue = 0;
     int resNumDiv = 0;  // number of divisors of result
 
-    for (int i = 1; i <= 50000; ++i) {
-        int countNumDiv = 0;
+    auto tpStart = mytool::HiResClock::now();
 
-        for (int j = 1; j <= i; ++j)
+    for (int i = RANGE_START; i <= RANGE_STOP; ++i) {
+        int numDiv = 0;
+
+        for (int j = i / 2; j > 0; --j)
             if (i % j == 0)
-                ++countNumDiv;
+                ++numDiv;
 
-        if (resNumDiv < countNumDiv) {
-            resNumDiv = countNumDiv;
-            resInt = i;
+        if (resNumDiv < numDiv) {
+            resNumDiv = numDiv;
+            resValue = i;
         }
     }
 
     auto timeSpan = mytool::HiResClock::getTimeSpan(tpStart);
 
-    cout << "The integer which has largest number of divisors is " << resInt << endl;
+    cout << "The integer which has largest number of divisors is " << resValue << endl;
     cout << "The largest number of divisor is " << resNumDiv << endl;
     cout << "Time elapsed = " << timeSpan.count() << endl;
 
