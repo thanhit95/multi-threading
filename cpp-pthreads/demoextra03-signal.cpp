4b43229f9e4a024a41189351350bba847b3c7f92
thanhit95 <thanh.it1995@gmail.com>
2021-06-15 15:19:33 +0700

    demo signal

---
+++ cpp-pthreads/demoextra03-signal.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <pthread.h>
+#include <unistd.h>
+#include <signal.h>
+using namespace std;
+
+
+
+void signalHandler(int sig) {
+    cout << "caught signal " << sig << endl;
+    // signal(SIGSEGV, signalHandler);
+}
+
+
+
+void* func(void *arg) {
+    cout << "foo" << endl;
+    sleep(5);
+    pthread_exit(nullptr);
+    return (void*)0;
+}
+
+
+
+int main()
+{
+    pthread_t tid;
+
+    signal(SIGSEGV, signalHandler); // register signal handler before going multithread
+
+    pthread_create(&tid, nullptr, func, nullptr);
+    sleep(1); // leave time for initialization
+
+    pthread_kill(tid, SIGSEGV);
+
+    pthread_join(tid, NULL);
+
+    cout << "bar" << endl;
+    return 0;
+}
