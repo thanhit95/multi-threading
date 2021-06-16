acfaffea53d48cc7bea3db8e0826731f02e92a4b
thanhit95 <thanh.it1995@gmail.com>
2021-06-16 16:13:01 +0700

    demo thread pool

---
+++ cpp-std/demo08-thread-pool.cpp
@@ -0,0 +1,12 @@
+/*
+THREAD POOL
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout << "\"Thread pool\" in std threads is not supported by default." << endl;
+    return 0;
+}
