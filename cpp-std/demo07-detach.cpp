5ba5d20237a8a8be28ad17a6fe1c38895986b730
thanhit95 <thanh.it1995@gmail.com>
2021-06-16 16:12:47 +0700

    demo detach

---
+++ cpp-std/demo07-detach.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+
+
+void routine() {
+    cout << "routine is starting..." << endl;
+
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    cout << "routine is exiting..." << endl;
+}
+
+
+
+int main() {
+    auto th = thread(routine);
+    th.detach();
+
+
+    // If I comment this statement, the thread routine will be forced into terminating with main thread
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+
+
+    cout << "program is terminating" << endl;
+    return 0;
+}
