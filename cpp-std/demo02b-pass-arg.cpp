5267092b92abdb25b96f8635aa391cca649fdd0f
thanhit95 <thanh.it1995@gmail.com>
2021-06-16 15:09:25 +0700

    reorganize: demo02b is passing const ref, demo02c is passing ref

--- cpp-std/demo02b-pass-arg.cpp
+++ cpp-std/demo02b-pass-arg.cpp
@@ -6,19 +6,7 @@ using namespace std;
 
 
 
-/*
-The arguments to the thread function are moved or copied by value.
-If a reference argument needs to be passed to the thread function,
-it has to be wrapped (e.g. with std::ref or std::cref).
-
-Passing references to thread functions may cause memory violation (e.g. when object is destroyed).
-By wrapping reference arguments with the class template std::reference_wrapper
-(using the function templates std::ref and std::cref), you explicitly express your intentions.
-*/
-
-
-
-void routine(string &msg) {
+void routine(const string &msg) {
     cout << msg << endl;
 }
 
@@ -28,10 +16,8 @@ int main() {
     string a = "foo";
     string b = "bar";
 
-    // auto th1 = thread(routine, a); // error
-
-    auto th1 = thread(routine, std::ref(a));
-    auto th2 = thread(routine, std::ref(b));
+    auto th1 = thread(routine, a);
+    auto th2 = thread(routine, b);
 
     th1.join();
     th2.join();
