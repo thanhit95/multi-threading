571e694407178d968571c920ffad91ce09f2955e
thanhit95 <thanh.it1995@gmail.com>
2021-06-20 09:54:49 +0700

    exercise dining philosophers

---
+++ cpp-std/ex04-dining-philosophers.cpp
@@ -0,0 +1,51 @@
+/*
+THE DINING PHILOSOPHERS PROBLEM
+*/
+
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+using namespace std;
+
+
+
+void funcPhilosopher(std::mutex chopstick[], int numPhilo, int idPhilo) {
+    int n = numPhilo;
+    int i = idPhilo;
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    chopstick[i].lock();
+    chopstick[(i + 1) % n].lock();
+
+    cout << "Philosopher #" << i << " is eating the rice" << endl;
+
+    chopstick[i].unlock();
+    chopstick[(i + 1) % n].unlock();
+}
+
+
+
+int main() {
+    constexpr int NUM_PHILOSOPHERS = 5;
+
+    std::mutex chopstick[NUM_PHILOSOPHERS];
+    std::thread lstTh[NUM_PHILOSOPHERS];
+
+
+    // CREATE THREADS
+    for (int i = 0; i < NUM_PHILOSOPHERS; ++i) {
+        lstTh[i] = std::thread(funcPhilosopher, chopstick, NUM_PHILOSOPHERS, i);
+    }
+
+
+    // JOIN THREADS
+    for (auto &&th : lstTh) {
+        th.join();
+    }
+
+
+    return 0;
+}
