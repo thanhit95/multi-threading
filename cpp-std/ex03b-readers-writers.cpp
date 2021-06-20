e40c1f899370bb6576972c40df789b13bd5a7263
thanhit95 <thanh.it1995@gmail.com>
2021-06-20 09:54:06 +0700

    exercise readers-writers

---
+++ cpp-std/ex03b-readers-writers.cpp
@@ -0,0 +1,124 @@
+/*
+THE READERS-WRITERS PROBLEM
+*/
+
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <semaphore>
+#include <chrono>
+#include "mytool-random.hpp"
+using namespace std;
+
+
+
+using TypeSemaphore = std::counting_semaphore<>;
+
+
+
+void funcWriter(
+    int *resource,
+    TypeSemaphore *semResource,
+    TypeSemaphore *semServiceQueue,
+    int timeDelay
+) {
+    std::this_thread::sleep_for(std::chrono::seconds(timeDelay));
+
+    semResource->acquire();
+
+    (*resource) = mytool::RandInt::staticGet() % 100;
+    cout << "Write " << (*resource) << endl;
+
+    semResource->release();
+}
+
+
+
+void funcReader(
+    int *resource,
+    TypeSemaphore *semResource,
+    int *readerCount,
+    std::mutex *mutReaderCount,
+    TypeSemaphore *semServiceQueue,
+    int timeDelay
+) {
+    std::this_thread::sleep_for(std::chrono::seconds(timeDelay));
+
+
+    // inrease reader count
+    mutReaderCount->lock();
+    (*readerCount) += 1;
+
+    if (1 == (*readerCount))
+        semResource->acquire();
+
+    mutReaderCount->unlock();
+
+
+    // do the reading
+    int data = (*resource);
+    cout << "Read " << data << endl;
+
+
+    // decrease reader count
+    mutReaderCount->lock();
+    (*readerCount) -= 1;
+
+    if (0 == (*readerCount))
+        semResource->release();
+
+    mutReaderCount->unlock();
+}
+
+
+
+int main() {
+    int resource = 0;
+    TypeSemaphore semResource(1);
+
+    int readerCount = 0;
+    std::mutex mutReaderCount;
+
+    TypeSemaphore semServiceQueue(1);
+
+
+    constexpr int NUM_READERS = 8;
+    constexpr int NUM_WRITERS = 6;
+
+    std::thread lstThReader[NUM_READERS];
+    std::thread lstThWriter[NUM_WRITERS];
+
+
+    // CREATE THREADS
+    for (auto &&th: lstThReader) {
+        th = std::thread(
+            funcReader,
+            &resource, &semResource, &readerCount, &mutReaderCount,
+            &semServiceQueue,
+            mytool::RandInt::staticGet() % 3
+        );
+    }
+
+    for (auto &&th: lstThWriter) {
+        th = std::thread(
+            funcWriter,
+            &resource, &semResource,
+            &semServiceQueue,
+            mytool::RandInt::staticGet() % 3
+        );
+    }
+
+
+    // JOIN THREADS
+    for (auto &&th: lstThReader) {
+        th.join();
+    }
+
+    for (auto &&th: lstThWriter) {
+        th.join();
+    }
+
+
+    return 0;
+}
