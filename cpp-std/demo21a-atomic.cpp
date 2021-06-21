fa910d9cd7ae8d352e7fc6a25260b04ea5440ebf
thanhit95 <thanh.it1995@gmail.com>
2021-06-21 11:32:02 +0700

    demo atomic by using volatile pitfall

---
+++ cpp-std/demo21a-atomic.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+using namespace std;
+
+
+
+volatile int counter = 0;
+
+
+
+void routine() {
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    counter += 1;
+}
+
+
+
+int main() {
+    auto thA = std::thread(routine);
+    auto thB = std::thread(routine);
+
+    thA.join();
+    thB.join();
+
+    cout << "counter = " << counter << endl; // unpredictable result
+    return 0;
+}
