7fd2e4498b56ebf916618ce5657f28f960591647
thanhit95 <thanh.it1995@gmail.com>
2021-06-22 09:02:03 +0700

    project sync: sync with cpp-std: add demo sleep

---
+++ cpp-pthreads/demo03-sleep.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <pthread.h>
+#include <unistd.h>
+using namespace std;
+
+
+
+void* routine(void* arg) {
+    auto name = (const char*)arg;
+
+    cout << name << " is sleeping" << endl;
+    sleep(2);
+    cout << name << " wakes up" << endl;
+
+    pthread_exit(nullptr);
+    return (void*)0;
+}
+
+
+
+int main() {
+    pthread_t tid1, tid2;
+    int ret = 0;
+
+    ret = pthread_create(&tid1, nullptr, routine, (void*)"foo");
+    ret = pthread_create(&tid2, nullptr, routine, (void*)"bar");
+
+    ret = pthread_join(tid1, nullptr);
+    ret = pthread_join(tid2, nullptr);
+
+    return 0;
+}
