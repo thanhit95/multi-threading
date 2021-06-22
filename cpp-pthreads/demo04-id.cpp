78bb6b3087c34e819030c7489cf46a06527d989f
thanhit95 <thanh.it1995@gmail.com>
2021-06-22 09:01:33 +0700

    project sync: sync with cpp-std

--- cpp-pthreads/demo04-id.cpp
+++ cpp-pthreads/demo04-id.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <pthread.h>
-
+#include <unistd.h>
 using namespace std;
 
 
 
-void *routine(void *) {
-    cout << "thread id is " << pthread_self() << endl;
+void* routine(void*) {
+    sleep(2);
+    cout << "Thread id is " << pthread_self() << endl;
 
     pthread_exit(nullptr);
     return (void*)0;
@@ -16,17 +17,16 @@ void *routine(void *) {
 
 int main() {
     pthread_t tid1, tid2;
-    int ret;
+    int ret = 0;
 
     ret = pthread_create(&tid1, nullptr, routine, nullptr);
     ret = pthread_create(&tid2, nullptr, routine, nullptr);
 
-    ret = pthread_join(tid1, nullptr);
-    ret = pthread_join(tid2, nullptr);
-
-    cout << endl;
     cout << "tid1 = " << tid1 << endl;
     cout << "tid2 = " << tid2 << endl;
+
+    ret = pthread_join(tid1, nullptr);
+    ret = pthread_join(tid2, nullptr);
 
     return 0;
 }
