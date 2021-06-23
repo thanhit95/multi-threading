3eb28e6f74808edd7fd59060cfe6ea4dffff650b
thanhit95 <thanh.it1995@gmail.com>
2021-06-23 09:56:52 +0700

    demo volatile

---
+++ cpp-pthreads/demo20-volatile.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <pthread.h>
+#include <unistd.h>
+using namespace std;
+
+
+
+volatile int stopped = false;
+
+
+
+void* routine(void*) {
+    while (false == stopped) {
+        cout << "Running..." << endl;
+        sleep(2);
+    }
+
+    pthread_exit(nullptr);
+    return (void*)0;
+}
+
+
+
+int main() {
+    pthread_t tid;
+    int ret = 0;
+
+    ret = pthread_create(&tid, nullptr, routine, nullptr);
+
+    sleep(6);
+
+    stopped = true;
+
+    ret = pthread_join(tid, nullptr);
+
+    return 0;
+}
