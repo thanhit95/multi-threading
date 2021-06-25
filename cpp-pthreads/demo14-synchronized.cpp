6b84cee8c6c1394d16dee1b8797a6c550433ad9d
thanhit95 <thanh.it1995@gmail.com>
2021-06-25 17:03:12 +0700

    project sync: sync with java-csharp: add demo synchronized

---
+++ cpp-pthreads/demo14-synchronized.cpp
@@ -0,0 +1,13 @@
+/*
+SYNCHRONIZED
+*/
+
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout << "C++ currently does not support \"synchronized block\" by default." << endl;
+    return 0;
+}
