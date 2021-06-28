210a5b8f02e4398881d8711fda1be3d96c3526e0
thanhit95 <thanh.it1995@gmail.com>
2021-06-28 17:20:38 +0700

    demo detach

---
+++ java/src/demo08/Program.java
@@ -0,0 +1,31 @@
+/*
+ * DETACH
+*/
+
+package demo08;
+
+
+
+public class Program {
+
+    public static void main(String[] args) throws InterruptedException {
+        var th = new Thread(() -> {
+            System.out.println("Routine is starting...");
+
+            try { Thread.sleep(2000); } catch (InterruptedException e) { }
+
+            System.out.println("Routine is exiting...");
+        });
+
+        th.setDaemon(true);
+        th.start();
+
+
+        // If I comment this statement, the thread routine will be forced into terminating with main thread
+        Thread.sleep(3000);
+
+
+        System.out.println("Program is terminating");
+    }
+
+}
