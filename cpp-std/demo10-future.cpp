0f0beed450bcb83a77a23346df397d41191c74d1
thanhit95 <thanh.it1995@gmail.com>
2021-06-29 10:04:28 +0700

    add demo future

---
+++ cpp-std/demo10-future.cpp
@@ -0,0 +1,41 @@
+/*
+FUTURE
+*/
+
+
+#include <iostream>
+#include <thread>
+#include <future>
+
+
+int main() {
+    // future from a packaged_task
+    std::packaged_task<int()> task([]{ return 7; });    // wrap the function
+    std::future<int> fut1 = task.get_future();          // get a future
+    std::thread th(std::move(task));                    // launch on a thread
+
+
+    // future from an async()
+    std::future<int> fut2 = std::async(std::launch::async, []{ return 8; });
+
+
+    // future from a promise
+    std::promise<int> prom;
+    std::future<int> fut3 = prom.get_future();
+    std::thread( [&prom]{ prom.set_value_at_thread_exit(9); }).detach();
+
+
+    std::cout << "Waiting..." << std::endl;
+    fut1.wait();
+    fut2.wait();
+    fut3.wait();
+    th.join();
+
+    std::cout << "Done!" << std::endl;
+
+    std::cout << "Results are: "
+              << fut1.get() << ' ' << fut2.get() << ' ' << fut3.get() << std::endl;
+
+
+    return 0;
+}
