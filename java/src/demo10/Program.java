0f42f36c6b12778522b129962cd0d546c798a636
thanhit95 <thanh.it1995@gmail.com>
2021-06-29 11:51:38 +0700

    add demo future

---
+++ java/src/demo10/Program.java
@@ -0,0 +1,31 @@
+/*
+ * FUTURE
+*/
+
+package demo10;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+
+
+public class Program {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        Future<String> fut = executor.submit(() -> "foo");
+
+        while (fut.isDone() == false) {
+            Thread.sleep(500);
+        }
+
+        executor.shutdown();
+
+        String result = fut.get();
+        System.out.println(result);
+    }
+
+}
