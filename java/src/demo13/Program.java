e86a441d9ed4e553d41e95d0f66235d491e86fe8
thanhit95 <thanh.it1995@gmail.com>
2021-07-01 21:19:21 +0700

    demo mutex

---
+++ java/src/demo13/Program.java
@@ -0,0 +1,18 @@
+/*
+ * MUTEX (Mutual Exclusion)
+ */
+
+package demo13;
+
+
+
+public class Program {
+
+    public static void main(String[] args) {
+        /*
+         * Unfortunately, Java does not support mutex by default.
+         * However, Java does support keyword "synchronized", which is closest to the term "mutex".
+         */
+    }
+
+}
