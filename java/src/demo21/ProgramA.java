5d73d8e962e25c27c92fa5f03673988f4b8eaeb7
thanhit95 <thanh.it1995@gmail.com>
2021-07-04 14:55:55 +0700

    demo condition variable

---
+++ java/src/demo21/ProgramA.java
@@ -0,0 +1,46 @@
+/*
+ * CONDITION VARIABLE
+*/
+
+package demo21;
+
+
+
+public class ProgramA {
+
+    public static void main(String[] args) {
+        var conditionVar = new Object();
+
+
+        Runnable foo = () -> {
+            try {
+                System.out.println("foo is waiting...");
+
+                synchronized(conditionVar) {
+                    // foo must own the conditionVar before using it
+                    conditionVar.wait();
+                }
+
+                System.out.println("foo resumed");
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+
+        Runnable bar = () -> {
+            try { Thread.sleep(3000); } catch (InterruptedException e) { }
+
+            synchronized(conditionVar) {
+                // bar must own the conditionVar before using it
+                conditionVar.notify();
+            }
+        };
+
+
+        new Thread(foo).start();
+        new Thread(bar).start();
+    }
+
+}
---
+++ java/src/demo21/ProgramD.java
@@ -0,0 +1,76 @@
+/*
+ * CONDITION VARIABLE
+*/
+
+package demo21;
+
+public class ProgramD {
+
+    public static void main(String[] args) {
+        new Foo().start();
+        new Eggs().start();
+    }
+
+}
+
+
+
+class Global {
+    public static Object conditionVar = new Object();
+
+    public static int counter = 0;
+
+    public static final int COUNT_HALT_01 = 3;
+    public static final int COUNT_HALT_02 = 6;
+    public static final int COUNT_DONE = 10;
+}
+
+
+
+// Write numbers 1-3 and 8-10 as permitted by eggs()
+class Foo extends Thread {
+    @Override
+    public void run() {
+        for (;;) {
+            synchronized(Global.conditionVar) {
+                try {
+                    Global.conditionVar.wait();
+
+                    Global.counter += 1;
+                    System.out.println("foo counter = " + Global.counter);
+
+                    if (Global.counter >= Global.COUNT_DONE) {
+                        return;
+                    }
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
+
+
+// Write numbers 4-7
+class Eggs extends Thread {
+    @Override
+    public void run() {
+        for (;;) {
+            synchronized(Global.conditionVar) {
+                if (Global.counter < Global.COUNT_HALT_01 || Global.counter > Global.COUNT_HALT_02) {
+                    Global.conditionVar.notify();
+                }
+                else {
+                    Global.counter += 1;
+                    System.out.println("eggs counter = " + Global.counter);
+                }
+
+                if (Global.counter >= Global.COUNT_DONE) {
+                    return;
+                }
+            }
+        }
+    }
+}
