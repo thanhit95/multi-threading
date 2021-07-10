d96933f1399fbddaf6ffa8f21d4c26d01b3f3428
thanhit95 <thanh.it1995@gmail.com>
2021-07-10 19:59:00 +0700

    ex producer-consumer

---
+++ java/src/ex02/ProgramB03.java
@@ -0,0 +1,68 @@
+/*
+ * THE PRODUCER-CONSUMER PROBLEM
+ *
+ * SOLUTION TYPE B: USING SEMAPHORE
+ *      Version B03: 2 fast producers, 1 slow consumer
+*/
+
+package ex02;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.Semaphore;
+
+
+
+public class ProgramB03 {
+
+    public static void main(String[] args) {
+        var semFill = new Semaphore(0);     // item produced
+        var semEmpty = new Semaphore(1);    // remaining space in queue
+
+        Queue<Integer> qProduct = new LinkedList<>();
+
+        new Thread(() -> funcProducer(semFill, semEmpty, qProduct, 0)).start();
+        new Thread(() -> funcProducer(semFill, semEmpty, qProduct, 1000)).start();
+
+        new Thread(() -> funcConsumer(semFill, semEmpty, qProduct)).start();
+    }
+
+
+    private static void funcProducer(Semaphore semFill, Semaphore semEmpty,
+                                     Queue<Integer> qProduct, int dataAddValue) {
+        int i = 1;
+
+        for (;; ++i) {
+            try {
+                semEmpty.acquire();
+
+                qProduct.add(i + dataAddValue);
+
+                semFill.release();
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private static void funcConsumer(Semaphore semFill, Semaphore semEmpty, Queue<Integer> qProduct) {
+        for (;;) {
+            try {
+                semFill.acquire();
+
+                int data = qProduct.remove();
+
+                System.out.println("Consumer " + data);
+                Thread.sleep(1000);
+
+                semEmpty.release();
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
