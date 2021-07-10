5f7c7f3657b7e8048f7a8b722040f49d621b6a4f
thanhit95 <thanh.it1995@gmail.com>
2021-07-10 19:59:20 +0700

    ex readers-writers

---
+++ java/src/ex03/ProgramB.java
@@ -0,0 +1,111 @@
+/*
+ * THE READERS-WRITERS PROBLEM
+ * Solution for the third readers-writers problem
+*/
+
+package ex03;
+
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+import java.util.stream.IntStream;
+
+
+
+public class ProgramB {
+
+    public static void main(String[] args) {
+        final int NUM_READERS = 8;
+        final int NUM_WRITERS = 6;
+
+
+        var rand = new Random();
+
+
+        var lstThReader = IntStream.range(0, NUM_READERS).mapToObj(i -> new Thread(() -> {
+            try {
+                funcReader(rand.nextInt(3));
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }));
+
+
+        var lstThWriter = IntStream.range(0, NUM_WRITERS).mapToObj(i -> new Thread(() -> {
+            try {
+                funcWriter(rand.nextInt(3));
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }));
+
+
+        lstThReader.forEach(Thread::start);
+        lstThWriter.forEach(Thread::start);
+    }
+
+
+    private static void funcWriter(int timeDelay) throws InterruptedException {
+        var rand = new Random();
+        Thread.sleep(1000 * timeDelay);
+
+        Global.semServiceQueue.acquire();
+
+        Global.semResource.acquire();
+
+        Global.semServiceQueue.release();
+
+        Global.resource = rand.nextInt(100);
+        System.out.println("Write " + Global.resource);
+
+        Global.semResource.release();
+    }
+
+
+    private static void funcReader(int timeDelay) throws InterruptedException {
+        Thread.sleep(1000 * timeDelay);
+
+
+        Global.semServiceQueue.acquire();
+
+
+        // inrease reader count
+        synchronized (Global.mutReaderCount) {
+            Global.readerCount += 1;
+
+            if (1 == Global.readerCount)
+                Global.semResource.acquire();
+        }
+
+
+        Global.semServiceQueue.release();
+
+
+        // do the reading
+        int data = Global.resource;
+        System.out.println("Read " + data);
+
+
+        // decrease reader count
+        synchronized (Global.mutReaderCount) {
+            Global.readerCount -= 1;
+
+            if (0 == Global.readerCount)
+                Global.semResource.release();
+        }
+    }
+
+
+
+    class Global {
+        static Semaphore semServiceQueue = new Semaphore(1);
+
+        static Semaphore semResource = new Semaphore(1);
+        static Object mutReaderCount = new Object();
+
+        static volatile int resource = 0;
+        static int readerCount = 0;
+    }
+
+}
