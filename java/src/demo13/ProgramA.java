7e813f4c1d09969c452769caa66095e6a02c8766
thanhit95 <thanh.it1995@gmail.com>
2021-07-11 17:15:26 +0700

    add demo: using a binary semaphore as a mutex

--- java/src/demo13/ProgramA.java
+++ java/src/demo13/ProgramA.java
@@ -6,12 +6,13 @@ package demo13;
 
 
 
-public class Program {
+public class ProgramA {
 
     public static void main(String[] args) {
         /*
          * Unfortunately, Java does not support mutex by default.
          * However, Java does support keyword "synchronized", which is closest to the term "mutex".
+         * Additionally, you can use a binary semaphore as a mutex.
          */
     }
 
