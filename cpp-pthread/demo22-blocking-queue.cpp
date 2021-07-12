986413ccc0c3fa13232288e3250e085be732edd7
thanhit95 <thanh.it1995@gmail.com>
2021-07-12 08:11:16 +0700

    demo blocking queue

---
+++ cpp-pthread/demo22-blocking-queue.cpp
@@ -0,0 +1,13 @@
+/*
+BLOCKING QUEUE
+*/
+
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout << "\"Blocking queue\" in C++ POSIX threading is not supported by default." << endl;
+    return 0;
+}
