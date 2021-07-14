72b939ffee7d225998186a89393e0aff123b46f4
thanhit95 <thanh.it1995@gmail.com>
2021-07-14 21:42:42 +0700

    update demo lock: add demo unique_lock

---
+++ cpp-std/demo13b-mutex-02.cpp
@@ -0,0 +1,63 @@
+/*
+MUTEX (Mutual Exclusion)
+
+- std::lock_guard will be locked only once on construction and unlocked on destruction.
+- In contrast to std::lock_guard, std::unique_lock can be:
+  + created without immediately locking, and
+  + unlocked at any point in its existence.
+
+Furthermore, C++17 introduces a new lock class called std::scoped_lock.
+The std::scoped_lock is a strictly superior version of std::lock_guard that
+locks an arbitrary number of mutexes all at once.
+*/
+
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+using namespace std;
+
+
+
+std::mutex mut;
+int counter = 0;
+
+
+
+void routineCounter() {
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    std::unique_lock<std::mutex> lk(mut);
+
+    for (int i = 0; i < 10000; ++i)
+        ++counter;
+
+    lk.unlock();
+
+    // do something...
+}
+
+
+
+int main() {
+    constexpr int NUM_THREADS = 3;
+    std::thread lstTh[NUM_THREADS];
+
+
+    counter = 0;
+
+
+    for (auto&& th : lstTh) {
+        th = std::thread(routineCounter);
+    }
+
+
+    for (auto&& th : lstTh) {
+        th.join();
+    }
+
+
+    cout << "counter = " << counter << endl;
+    return 0;
+}
