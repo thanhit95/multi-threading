4edc026a322f33475c1deac172e88e5afeeb4e00
thanhit95 <thanh.it1995@gmail.com>
2021-10-27 22:11:02 +0700

    demo thread-local storage

---
+++ java/src/demo23/ProgramA.java
@@ -0,0 +1,66 @@
+/*
+ * THREAD-LOCAL STORAGE
+*/
+
+package demo23;
+
+import java.util.stream.IntStream;
+
+
+
+public class ProgramA {
+
+    public static void main(String[] args) throws InterruptedException {
+        final int NUM_THREADS = 3;
+
+
+        var lstTh = IntStream.range(0, NUM_THREADS).mapToObj(t -> new Thread(() -> {
+            try { Thread.sleep(1000); } catch (InterruptedException e) { }
+
+            for (int i = 0; i < 1000; ++i)
+                MyTask.increaseCounter();
+
+            System.out.println("Thread " + t + " gives counter = " + MyTask.thlCounter.get().value);
+        })).toList();
+
+
+        for (var th : lstTh)
+            th.start();
+
+        for (var th : lstTh)
+            th.join();
+
+
+        /*
+         * By using thread-local storage, each thread has its own counter.
+         * So, the counter in one thread is completely independent of each other.
+         *
+         * Thread-local storage helps us to AVOID SYNCHRONIZATION.
+         */
+    }
+
+
+
+    static class Counter {
+        public int value = 0;
+    }
+
+
+
+    static class MyTask {
+        public static ThreadLocal<Counter> thlCounter = new ThreadLocal<>();
+
+        public static void increaseCounter() {
+            Counter counter = thlCounter.get();
+
+            if (null == counter) {
+                // first time getting counter in a thread
+                counter = new Counter();
+                thlCounter.set(counter);
+            }
+
+            ++counter.value;
+        }
+    }
+
+}
