d99e90e9e966c64025417f4ab4eba61be1b8ded6
thanhit95 <thanh.it1995@gmail.com>
2022-01-16 16:20:17 +0700

    use std::unique_lock to demonstrate synchronized blocks

--- cpp-std/demo14-synchronized-block.cpp
+++ cpp-std/demo14-synchronized-block.cpp
@@ -1,13 +1,62 @@
 /*
 SYNCHRONIZED BLOCKS
+
+Synchronized blocks in C++ std threading are not supported by default.
+To demonstate synchronized blocks, I use std::unique_lock (or std::lock_guard, std::scoped_lock).
+
+Now, let's see the code:
+    {
+        std::unique_lock lk(mut);
+        // do something in the critical section
+    }
+
+The code block above is protected by a lock/mutex. That means it is synchronized on thread execution.
+This code block is called "the synchronized block".
 */
 
 
 #include <iostream>
+#include <chrono>
+#include <thread>
+#include <mutex>
 using namespace std;
 
 
+
+std::mutex mut;
+int counter = 0;
+
+
+
+void doTask() {
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    // This is the "synchronized block"
+    {
+        std::unique_lock<std::mutex> lk(mut);
+
+        for (int i = 0; i < 10000; ++i)
+            ++counter;
+    }
+}
+
+
+
 int main() {
-    cout << "C++ currently does not support \"synchronized blocks\" by default." << endl;
+    constexpr int NUM_THREADS = 3;
+    std::thread lstTh[NUM_THREADS];
+
+
+    for (auto&& th : lstTh) {
+        th = std::thread(doTask);
+    }
+
+
+    for (auto&& th : lstTh) {
+        th.join();
+    }
+
+
+    cout << "counter = " << counter << endl;
     return 0;
 }
