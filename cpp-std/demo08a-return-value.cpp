822b8238c0100ff3b2abcf67b8ac82daf133f551
thanhit95 <thanh.it1995@gmail.com>
2022-03-10 23:11:55 +0700

    add the example code to return value using the references

--- cpp-std/demo08a-return-value.cpp
+++ cpp-std/demo08a-return-value.cpp
@@ -16,17 +16,26 @@ void doubleValue(int arg, int* res) {
 
 
 
+void squareValue(int arg, int& res) {
+    res = arg * arg;
+}
+
+
+
 int main() {
-    int result[2];
+    int result[3];
 
     auto thFoo = std::thread(doubleValue, 5, &result[0]);
     auto thBar = std::thread(doubleValue, 80, &result[1]);
+    auto thEgg = std::thread(squareValue, 7, std::ref(result[2]));
 
     thFoo.join();
     thBar.join();
+    thEgg.join();
 
     cout << result[0] << endl;
     cout << result[1] << endl;
+    cout << result[2] << endl;
 
     return 0;
 }
