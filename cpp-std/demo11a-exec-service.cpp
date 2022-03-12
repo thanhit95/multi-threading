190263b245a1528ccb0bd3d91da44514ca5e4b73
thanhit95 <thanh.it1995@gmail.com>
2022-03-13 00:48:42 +0700

    use mylib::ExecService for the demonstration

---
+++ cpp-std/demo11a-exec-service.cpp
@@ -0,0 +1,49 @@
+/*
+EXECUTOR SERVICES AND THREAD POOLS
+
+Executor services in C++ std threading are not supported by default.
+So, I use mylib::ExecService for this demonstration.
+*/
+
+
+#include <iostream>
+#include "mylib-exec-service.hpp"
+using namespace std;
+
+
+
+void doTask() {
+    cout << "Hello the Executor Service" << endl;
+}
+
+
+
+class MyFunctor {
+public:
+    void operator()() {
+        cout << "Hello Multithreading" << endl;
+    }
+};
+
+
+
+int main() {
+    // INIT THE EXECUTOR SERVICE WITH 2 THREADS
+    auto execService = mylib::ExecService();
+    execService.init(2);
+
+
+    // SUBMIT
+    execService.submit([] { cout << "Hello World" << endl; });
+
+    execService.submit(doTask);
+
+    execService.submit(MyFunctor());
+
+
+    // WAIT FOR THE COMPLETION OF ALL TASKS AND SHUTDOWN EXECUTOR SERVICE
+    execService.waitTaskDone();
+    execService.shutdown();
+
+    return 0;
+}
