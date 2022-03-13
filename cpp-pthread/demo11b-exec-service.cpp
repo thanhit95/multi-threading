aada57de990e9e40341783724e2407fba2eed6e1
thanhit95 <thanh.it1995@gmail.com>
2022-03-13 10:04:28 +0700

    use mylib::ExecService for the demonstration

---
+++ cpp-pthread/demo11b-exec-service.cpp
@@ -0,0 +1,40 @@
+/*
+EXECUTOR SERVICES AND THREAD POOLS
+
+Executor services in C++ POSIX threading are not supported by default.
+So, I use mylib::ExecService for this demonstration.
+*/
+
+
+#include <iostream>
+#include <unistd.h>
+#include "../cpp-std/mylib-exec-service.hpp"
+using namespace std;
+
+
+
+int main() {
+    constexpr int NUM_THREADS = 2;
+    constexpr int NUM_TASKS = 5;
+
+    auto execService = mylib::ExecService();
+    execService.init(NUM_THREADS);
+
+    for (int i = 0; i < NUM_TASKS; ++i) {
+        execService.submit([=] {
+            char id = 'A' + i;
+            std::cout << "Task " << id << " is starting" << std::endl;
+            sleep(3);
+            std::cout << "Task " << id << " is completed" << std::endl;
+        });
+    }
+
+    std::cout << "All tasks are submitted" << std::endl;
+
+    execService.waitTaskDone();
+    std::cout << "All tasks are completed" << std::endl;
+
+    execService.shutdown();
+
+    return 0;
+}
