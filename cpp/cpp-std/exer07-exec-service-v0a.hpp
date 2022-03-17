b3df63cebf97f5631cc9b16e50e355992a0a3a57
thanhit95 <thanh.it1995@gmail.com>
2022-03-17 23:09:30 +0700

    add exer executor service version 0: use a blocking queue

---
+++ cpp/cpp-std/exer07-exec-service-v0a.hpp
@@ -0,0 +1,94 @@
+/*
+MY EXECUTOR SERVICE
+
+Version 0A: The easiest executor service
+- It uses a blocking queue as underlying mechanism.
+*/
+
+
+
+#ifndef _MY_EXEC_SERVICE_V0A_HPP_
+#define _MY_EXEC_SERVICE_V0A_HPP_
+
+
+
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <thread>
+#include "mylib-blockingqueue.hpp"
+#include "exer07-exec-service-itask.hpp"
+
+
+
+class MyExecServiceV0A {
+
+private:
+    int numThreads = 0;
+    std::vector<std::thread> lstTh;
+    mylib::BlockingQueue<ITask*> taskPending;
+
+
+public:
+    MyExecServiceV0A(int numThreads) : taskPending(1000) {
+        init(numThreads);
+    }
+
+
+    MyExecServiceV0A(const MyExecServiceV0A& other) = delete;
+    MyExecServiceV0A(const MyExecServiceV0A&& other) = delete;
+    void operator=(const MyExecServiceV0A& other) = delete;
+    void operator=(const MyExecServiceV0A&& other) = delete;
+
+
+private:
+    void init(int numThreads) {
+        this->numThreads = numThreads;
+        lstTh.resize(numThreads);
+
+        for (auto&& th : lstTh) {
+            th = std::thread(threadWorkerFunc, this);
+        }
+    }
+
+
+public:
+    void submit(ITask* task) {
+        taskPending.add(task);
+    }
+
+
+    void waitTaskDone() {
+        // This ExecService is too simple,
+        // so there is no implementation for waitTaskDone()
+        std::this_thread::sleep_for(std::chrono::seconds(11)); // fake behaviour
+    }
+
+
+    void shutdown() {
+        // This ExecService is too simple,
+        // so there is no implementation for shutdown()
+        std::cout << "No implementation for shutdown()." << std::endl;
+        std::cout << "You need to exit the app manually." << std::endl;
+    }
+
+
+private:
+    static void threadWorkerFunc(MyExecServiceV0A* thisPtr) {
+        auto&& taskPending = thisPtr->taskPending;
+        ITask* task = nullptr;
+
+        for (;;) {
+            // WAIT FOR AN AVAILABLE PENDING TASK
+            task = taskPending.take();
+
+            // DO THE TASK
+            task->run();
+        }
+    }
+
+};
+
+
+
+#endif // _MY_EXEC_SERVICE_V0A_HPP_
---
+++ cpp/cpp-std/exer07-exec-service-v0b.hpp
@@ -0,0 +1,131 @@
+/*
+MY EXECUTOR SERVICE
+
+Version 0B: The easiest executor service
+- It uses a blocking queue as underlying mechanism.
+- It supports waitTaskDone() and shutdown().
+*/
+
+
+
+#ifndef _MY_EXEC_SERVICE_V0B_HPP_
+#define _MY_EXEC_SERVICE_V0B_HPP_
+
+
+
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <thread>
+#include <atomic>
+#include "mylib-blockingqueue.hpp"
+#include "exer07-exec-service-itask.hpp"
+
+
+
+class MyExecServiceV0B {
+
+private:
+    int numThreads = 0;
+    std::vector<std::thread> lstTh;
+
+    mylib::BlockingQueue<ITask*> taskPending;
+    std::atomic_int32_t counterTaskRunning;
+
+    volatile bool forceThreadShutdown;
+
+    const class : ITask {
+        void run() override { }
+    }
+    emptyTask;
+
+
+public:
+    MyExecServiceV0B(int numThreads) : taskPending(1000) {
+        init(numThreads);
+    }
+
+
+    MyExecServiceV0B(const MyExecServiceV0B& other) = delete;
+    MyExecServiceV0B(const MyExecServiceV0B&& other) = delete;
+    void operator=(const MyExecServiceV0B& other) = delete;
+    void operator=(const MyExecServiceV0B&& other) = delete;
+
+
+private:
+    void init(int numThreads) {
+        this->numThreads = numThreads;
+        lstTh.resize(numThreads);
+        counterTaskRunning = 0;
+        forceThreadShutdown = false;
+
+        for (auto&& th : lstTh) {
+            th = std::thread(threadWorkerFunc, this);
+        }
+    }
+
+
+public:
+    void submit(ITask* task) {
+        taskPending.add(task);
+    }
+
+
+    void waitTaskDone() {
+        // This ExecService is too simple,
+        // so there is no good implementation for waitTaskDone()
+
+        // Note: Bad implementation
+        while (taskPending.size() > 0 || counterTaskRunning.load() > 0) {
+            std::this_thread::yield(); // or sleep for a while...
+        }
+    }
+
+
+    void shutdown() {
+        forceThreadShutdown = true;
+        taskPending.clear();
+
+        // Invoke blocked threads by adding "empty" tasks
+        for (int i = 0; i < numThreads; ++i) {
+            taskPending.put( (ITask* const) &emptyTask );
+        }
+
+        for (auto&& th : lstTh) {
+            th.join();
+        }
+
+        numThreads = 0;
+        lstTh.clear();
+    }
+
+
+private:
+    static void threadWorkerFunc(MyExecServiceV0B* thisPtr) {
+        auto&& taskPending = thisPtr->taskPending;
+        auto&& counterTaskRunning = thisPtr->counterTaskRunning;
+        auto&& forceThreadShutdown = thisPtr->forceThreadShutdown;
+
+        ITask* task = nullptr;
+
+        for (;;) {
+            // WAIT FOR AN AVAILABLE PENDING TASK
+            task = taskPending.take();
+
+            // If shutdown() was called, then exit the function
+            if (forceThreadShutdown) {
+                break;
+            }
+
+            // DO THE TASK
+            ++counterTaskRunning;
+            task->run();
+            --counterTaskRunning;
+        }
+    }
+
+};
+
+
+
+#endif // _MY_EXEC_SERVICE_V0B_HPP_
