1811ad74d515919f4594e05e0529b19cfc69f344
thanhit95 <thanh.it1995@gmail.com>
2022-03-17 23:03:26 +0700

    add mylib: blockingqueue and executor service

---
+++ cpp/cpp-pthread/mylib-blockingqueue.hpp
@@ -0,0 +1,170 @@
+/******************************************************
+*
+* File name:    mylib-blockingqueue.hpp
+*
+* Author:       Name:   Thanh Nguyen
+*               Email:  thanh.it1995(at)gmail(dot)com
+*
+* License:      3-Clause BSD License
+*
+* Description:  The blocking queue implementation in C++11 POSIX threading
+*
+******************************************************/
+
+
+
+#ifndef _MYLIB_BLOCKING_QUEUE_HPP_
+#define _MYLIB_BLOCKING_QUEUE_HPP_
+
+
+
+#include <queue>
+#include <pthread.h>
+
+
+
+namespace mylib
+{
+
+
+
+template <typename T>
+class BlockingQueue {
+
+private:
+    pthread_cond_t condEmpty = PTHREAD_COND_INITIALIZER;
+    pthread_cond_t condFull = PTHREAD_COND_INITIALIZER;
+    pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
+
+    size_t capacity;
+    std::queue<T> q;
+
+    struct PendingData {
+        BlockingQueue<T> * thisPtr;
+        const T data;
+
+        PendingData(BlockingQueue<T> * thisPtr, const T data)
+            : thisPtr(thisPtr), data(data) { }
+    };
+
+
+public:
+    BlockingQueue(size_t capacity) : capacity(capacity) {
+    }
+
+
+    BlockingQueue() : BlockingQueue(1) {
+    }
+
+
+    ~BlockingQueue() {
+        pthread_cond_destroy(&condEmpty);
+        pthread_cond_destroy(&condFull);
+        pthread_mutex_destroy(&mut);
+    }
+
+
+    BlockingQueue(const BlockingQueue& other) = delete;
+    BlockingQueue(const BlockingQueue&& other) = delete;
+    void operator=(const BlockingQueue& other) = delete;
+    void operator=(const BlockingQueue&& other) = delete;
+
+
+    bool empty() const {
+        return q.empty();
+    }
+
+
+    size_t size() const {
+        return q.size();
+    }
+
+
+    // sync enqueue
+    void put(const T& value) {
+        int ret = 0;
+
+        ret = pthread_mutex_lock(&mut);
+
+        while (capacity == q.size()) {
+            ret = pthread_cond_wait(&condFull, &mut);
+        }
+
+        q.push(value);
+
+        ret = pthread_mutex_unlock(&mut);
+        ret = pthread_cond_signal(&condEmpty);
+    }
+
+
+    // sync dequeue
+    T take() {
+        T result;
+        int ret = 0;
+
+        ret = pthread_mutex_lock(&mut);
+
+        while (0 == q.size()) {
+            // Queue is empty, must wait for 'put'
+            ret = pthread_cond_wait(&condEmpty, &mut);
+        }
+
+        result = q.front();
+        q.pop();
+
+        ret = pthread_mutex_unlock(&mut);
+        ret = pthread_cond_signal(&condFull);
+
+        return result;
+    }
+
+
+    // async enqueue
+    void add(const T& value) {
+        // Note: For asynchronous operations, we should use a long-live background thread
+        // instead of using a temporary thread
+        pthread_t tid;
+        auto arg = new PendingData(this, value);
+        pthread_create(&tid, nullptr, &BlockingQueue<T>::putPending, arg);
+        pthread_detach(tid);
+    }
+
+
+    // returns false if queue is empty, otherwise returns true and assigns the result
+    bool peek(T& result) const {
+        if (q.empty()) {
+            return false;
+        }
+
+        result = q.front();
+        return true;
+    }
+
+
+    void clear() {
+        int ret;
+        ret = pthread_mutex_lock(&mut);
+        std::queue<T>().swap(q);
+        ret = pthread_mutex_unlock(&mut);
+    }
+
+
+private:
+    static void* putPending(void* argVoid) {
+        auto arg = (PendingData*) argVoid;
+        arg->thisPtr->put(arg->data);
+        delete arg;
+        arg = nullptr;
+        pthread_exit(nullptr);
+        return nullptr;
+    }
+
+}; // BlockingQueue
+
+
+
+} // namespace mylib
+
+
+
+#endif // _MYLIB_BLOCKING_QUEUE_HPP_
---
+++ cpp/cpp-pthread/mylib-execservice.hpp
@@ -0,0 +1,213 @@
+/******************************************************
+*
+* File name:    mylib-execservice.hpp
+*
+* Author:       Name:   Thanh Nguyen
+*               Email:  thanh.it1995(at)gmail(dot)com
+*
+* License:      3-Clause BSD License
+*
+* Description:  The executor service implementation in C++11 POSIX threading
+*
+******************************************************/
+
+
+
+/*
+Copy code from "MyExecServiceV1B"
+*/
+
+
+#ifndef _MYLIB_EXEC_SERVICE_HPP_
+#define _MYLIB_EXEC_SERVICE_HPP_
+
+
+
+#include <vector>
+#include <queue>
+#include <functional>
+#include <pthread.h>
+
+
+
+namespace mylib {
+
+
+
+class ExecService {
+
+public:
+    using taskFunc = std::function<void()>;
+
+
+private:
+    int numThreads = 0;
+    std::vector<pthread_t> lstTh;
+
+    std::queue<taskFunc> taskPending;
+    pthread_mutex_t mutTaskPending = PTHREAD_MUTEX_INITIALIZER;
+    pthread_cond_t condTaskPending = PTHREAD_COND_INITIALIZER;
+
+    int counterTaskRunning;
+    pthread_mutex_t mutTaskRunning = PTHREAD_MUTEX_INITIALIZER;
+    pthread_cond_t condTaskRunning = PTHREAD_COND_INITIALIZER;
+
+    volatile bool forceThreadShutdown;
+
+
+public:
+    ExecService(int numThreads) {
+        init(numThreads);
+    }
+
+
+    ExecService(const ExecService& other) = delete;
+    ExecService(const ExecService&& other) = delete;
+    void operator=(const ExecService& other) = delete;
+    void operator=(const ExecService&& other) = delete;
+
+
+private:
+    void init(int numThreads) {
+        // shutdown();
+
+        mutTaskPending = PTHREAD_MUTEX_INITIALIZER;
+        condTaskPending = PTHREAD_COND_INITIALIZER;
+
+        mutTaskRunning = PTHREAD_MUTEX_INITIALIZER;
+        condTaskRunning = PTHREAD_COND_INITIALIZER;
+
+        this->numThreads = numThreads;
+        lstTh.resize(numThreads);
+        counterTaskRunning = 0;
+        forceThreadShutdown = false;
+
+        for (auto&& th : lstTh) {
+            pthread_create(&th, nullptr, threadWorkerFunc, this);
+        }
+    }
+
+
+public:
+    void submit(taskFunc task) {
+        pthread_mutex_lock(&mutTaskPending);
+        taskPending.push(task);
+        pthread_mutex_unlock(&mutTaskPending);
+
+        pthread_cond_signal(&condTaskPending);
+    }
+
+
+    void waitTaskDone() {
+        bool done = false;
+
+        for (;;) {
+            pthread_mutex_lock(&mutTaskPending);
+
+            if (0 == taskPending.size()) {
+                pthread_mutex_lock(&mutTaskRunning);
+
+                while (counterTaskRunning > 0)
+                    pthread_cond_wait(&condTaskRunning, &mutTaskRunning);
+
+                // no pending task and no running task
+                done = true;
+
+                pthread_mutex_unlock(&mutTaskRunning);
+            }
+
+            pthread_mutex_unlock(&mutTaskPending);
+
+            if (done) {
+                break;
+            }
+        }
+    }
+
+
+    void shutdown() {
+        pthread_mutex_lock(&mutTaskPending);
+
+        forceThreadShutdown = true;
+        std::queue<taskFunc>().swap(taskPending);
+
+        pthread_mutex_unlock(&mutTaskPending);
+
+        pthread_cond_broadcast(&condTaskPending);
+
+        for (auto&& th : lstTh) {
+            pthread_join(th, nullptr);
+        }
+
+        numThreads = 0;
+        lstTh.clear();
+
+        pthread_mutex_destroy(&mutTaskPending);
+        pthread_cond_destroy(&condTaskPending);
+        pthread_mutex_destroy(&mutTaskRunning);
+        pthread_cond_destroy(&condTaskRunning);
+    }
+
+
+private:
+    static void* threadWorkerFunc(void* argVoid) {
+        auto thisPtr = (ExecService*) argVoid;
+
+        auto&& taskPending = thisPtr->taskPending;
+        auto&& mutTaskPending = thisPtr->mutTaskPending;
+        auto&& condTaskPending = thisPtr->condTaskPending;
+
+        auto&& counterTaskRunning = thisPtr->counterTaskRunning;
+        auto&& mutTaskRunning = thisPtr->mutTaskRunning;
+        auto&& condTaskRunning = thisPtr->condTaskRunning;
+
+        auto&& forceThreadShutdown = thisPtr->forceThreadShutdown;
+
+
+        for (;;) {
+            // WAIT FOR AN AVAILABLE PENDING TASK
+            pthread_mutex_lock(&mutTaskPending);
+
+            while (0 == taskPending.size() and false == forceThreadShutdown) {
+                pthread_cond_wait(&condTaskPending, &mutTaskPending);
+            }
+
+            if (forceThreadShutdown) {
+                pthread_mutex_unlock(&mutTaskPending);
+                break;
+            }
+
+            // GET THE TASK FROM THE PENDING QUEUE
+            auto task = taskPending.front();
+            taskPending.pop();
+
+            ++counterTaskRunning;
+
+            pthread_mutex_unlock(&mutTaskPending);
+
+            // DO THE TASK
+            task();
+
+            pthread_mutex_lock(&mutTaskRunning);
+
+            --counterTaskRunning;
+            if (0 == counterTaskRunning) {
+                pthread_cond_signal(&condTaskRunning);
+            }
+
+            pthread_mutex_unlock(&mutTaskRunning);
+        }
+
+        pthread_exit(nullptr);
+        return nullptr;
+    }
+
+}; // ExecService
+
+
+
+} // namespace mylib
+
+
+
+#endif // _MYLIB_EXEC_SERVICE_HPP_
