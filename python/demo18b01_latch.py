04c1f196c6db7780ba1db04a3fb178a1ba0c2d0e
thanhit95 <thanh.it1995@gmail.com>
2022-03-20 10:16:08 +0700

    add demo latches using mylib_latch

---
+++ python/demo18b01_latch.py
@@ -0,0 +1,35 @@
+'''
+BARRIERS AND LATCHES
+Version B: Count-down latches
+
+Count-down latches in Python are not supported by default.
+So, I use mylib_latch for this demonstration.
+'''
+
+import time
+import threading
+from mylib_latch import CountDownLatch
+
+
+
+def process_request(user_name: str, time_wait: int):
+    time.sleep(time_wait)
+
+    print(f'Get request from {user_name}')
+
+    sync_point.count_down()
+    sync_point.wait()
+
+    print(f'Done {user_name}')
+
+
+
+lstarg = [
+    ('lorem', 1),
+    ('ipsum', 2),
+    ('dolor', 3)
+]
+
+sync_point = CountDownLatch(count=3)
+
+_ = [ threading.Thread(target=process_request, args=arg).start() for arg in lstarg ]
---
+++ python/demo18b02_latch.py
@@ -0,0 +1,39 @@
+'''
+BARRIERS AND LATCHES
+Version B: Count-down latches
+
+Main thread waits for 3 child threads to get enough data to progress.
+
+Count-down latches in Python are not supported by default.
+So, I use mylib_latch for this demonstration.
+'''
+
+import time
+import threading
+from mylib_latch import CountDownLatch
+
+
+
+def do_task(message: str, time_wait: int):
+    time.sleep(time_wait)
+
+    print(message)
+    sync_point.count_down()
+
+    time.sleep(8)
+    print('Cleanup')
+
+
+
+lstarg = [
+    ('Send request to egg.net to get data', 6),
+    ('Send request to foo.org to get data', 2),
+    ('Send request to bar.com to get data', 4)
+]
+
+sync_point = CountDownLatch(count=len(lstarg))
+
+_ = [ threading.Thread(target=do_task, args=arg).start() for arg in lstarg ]
+
+sync_point.wait()
+print('\nNow we have enough data to progress to next step\n')
